7431a52c7da9f377f0ff60b4b2f613328703d7e8
megazol <50046183+megazol3000@users.noreply.github.com>
2020-08-11 22:54:52 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+  	items: 1,
+  	autoWidth: true,
+  	nav: true,
+  	center: true,
+  	autoplay: true,
+  	smartSpeed: 600,
+  	loop: true,
+  });
+});
---
+++ js/slick.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+  $('.review-slider').slick({
+    setting-name: setting-value
+  });
+});
